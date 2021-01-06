79da596af8c6f6698875d3fdba85b1ccc3211eb4
Fellno <meeyfellno@gmail.com>
2021-01-06 17:33:56 +0800

    Refit for new sklearn

--- Code/Day 1_Data_Preprocessing.py
+++ Code/Day 1_Data_Preprocessing.py
@@ -1,42 +1,45 @@
-#Day 1: Data Prepocessing
+# Day 1: Data Prepocessing
 
-#Step 1: Importing the libraries
+# Step 1: Importing the libraries
 import numpy as np
 import pandas as pd
 
-#Step 2: Importing dataset
+# Step 2: Importing dataset
 dataset = pd.read_csv('../datasets/Data.csv')
-X = dataset.iloc[ : , :-1].values
-Y = dataset.iloc[ : , 3].values
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
 print("Step 2: Importing dataset")
 print("X")
 print(X)
 print("Y")
 print(Y)
 
-#Step 3: Handling the missing data
+# Step 3: Handling the missing data
 # If you use the newest version of sklearn, use the lines of code commented out
-# from sklearn.impute import SimpleImputer
-# imputer = SimpleImputer(missing_values="NaN", strategy="mean")
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
-imputer = imputer.fit(X[ : , 1:3])
-X[ : , 1:3] = imputer.transform(X[ : , 1:3])
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+# from sklearn.preprocessing import Imputer
+# imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 print("---------------------")
 print("Step 3: Handling the missing data")
 print("step2")
 print("X")
 print(X)
 
-#Step 4: Encoding categorical data
+# Step 4: Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X = LabelEncoder()
-X[ : , 0] = labelencoder_X.fit_transform(X[ : , 0])
-#Creating a dummy variable
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
+from sklearn.compose import ColumnTransformer
+
+# labelencoder_X = LabelEncoder()
+# X[ : , 0] = labelencoder_X.fit_transform(X[ : , 0])
+# Creating a dummy variable
+ct = ColumnTransformer([("Country", OneHotEncoder(), [0])], remainder="passthrough")
+X = ct.fit_transform(X)
 labelencoder_Y = LabelEncoder()
-Y =  labelencoder_Y.fit_transform(Y)
+Y = labelencoder_Y.fit_transform(Y)
 print("---------------------")
 print("Step 4: Encoding categorical data")
 print("X")
@@ -44,9 +47,10 @@ print(X)
 print("Y")
 print(Y)
 
-#Step 5: Splitting the datasets into training sets and Test sets
+# Step 5: Splitting the datasets into training sets and Test sets
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 print("---------------------")
 print("Step 5: Splitting the datasets into training sets and Test sets")
 print("X_train")
@@ -58,8 +62,9 @@ print(Y_train)
 print("Y_test")
 print(Y_test)
 
-#Step 6: Feature Scaling
+# Step 6: Feature Scaling
 from sklearn.preprocessing import StandardScaler
+
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
